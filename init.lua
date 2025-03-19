c51005c710e60fd868845b5ea2e4071eed665c78
Victor Martinelli <victorlcm93@gmail.com>
2025-03-19 13:22:38 -0300

    fix: typescript formatter

--- init.lua
+++ init.lua
@@ -794,9 +794,9 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         go = { 'gofmt', 'goimports' },
-        typescript = { { 'prettierd', 'prettier' } },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { { 'prettierd', 'prettier', stop_after_fist = true } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
         python = function(bufnr)
           if require('conform').get_formatter_info('ruff_format', bufnr).available then
             return { 'ruff_format' }
